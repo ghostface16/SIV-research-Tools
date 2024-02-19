de477f9de1e06b9ce0d3ebf7d3cfa8f4a3ee13cc
ghostface16 <136005675+ghostface16@users.noreply.github.com>
2024-02-19 14:02:39 -0500

    Update exce.py

--- exce.py
+++ exce.py
@@ -5,5 +5,5 @@ df_file = sys.argv[2]
 df = pd.read_csv(df_file, index_col=0)
 nrep_par = int(sys.argv[1])
 
-result = run_Kstar_perm(df, pairwise=False, compute_random_Ks=True, 
-                        size_Ki_samp=1000,  n_permut=1000, prop_to_permut=0.2, nrep_par=nrep_par)
+result = run_Kstar_perm(df,  nrep_par=nrep_par, pairwise=False, compute_random_Ks=True, 
+                        size_Ki_samp=1000,  n_permut=1000, prop_to_permut=0.2)
