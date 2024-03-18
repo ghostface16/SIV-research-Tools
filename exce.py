af8369a8ebcc5d11547204223d0cfb619eb1f404
ghostface16 <136005675+ghostface16@users.noreply.github.com>
2024-03-18 16:02:00 -0400

    Update exce.py

--- exce.py
+++ exce.py
@@ -10,5 +10,5 @@ nrep_par = int(sys.argv[1])
 if len(sys.argv)>3:
     csv_path = sys.argv[3]
 
-result = run_Kstar_perm(df, nrep_par=nrep_par, pairwise=True, compute_random_Ks=True, csv_dump=True, 
+result = run_Kstar_perm(df, nrep_par=nrep_par, pairwise=True, compute_random_Ks=False, csv_dump=True, 
                         grouping_index= -1, size_Ki_samp=1000,  n_permut=1000, prop_to_permut=0.2, sep="_")
